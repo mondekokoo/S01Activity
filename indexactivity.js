93a400c9b234d2702ea169e150b59f6dbcd81eb8
mondekokoo <tbr0718@dlsud.edu.ph>
2023-03-20 23:28:09 +0800

    230320

---
+++ indexactivity.js
@@ -0,0 +1,21 @@
+console.log("Hello, Welcome to De La Salle University!");
+
+let firstName = 'Brian Christian';
+let lastName = 'Torrijos';
+let age = 25;
+let hobbies = ["reading", "playing", "gym", "listening to music", "watching shows"];
+let homeAddress = {
+    houseNumber: "Blk1 lot 28 Ph 2",
+    street: "La Cienega st.",
+    villageNSubdivision: "California West Hills ",
+    barangay: "Buhay na Tubig",
+    city: "Imus",
+    province: "Cavite",
+    zipcode: 4103
+};
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(hobbies);
+console.log(homeAddress);
